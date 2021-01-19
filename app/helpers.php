c5bf52f3c034e09b8f03ecf622306c229f434390
sh-anna <62933455+sh-anna@users.noreply.github.com>
2021-01-19 10:29:11 +0200

    Add files via upload

--- app/helpers.php
+++ app/helpers.php
@@ -10,3 +10,18 @@ require_once 'db_config.php';
 function old($fn){
     return $_REQUEST[$fn] ?? '';
 }
+/*
+* edd comment
+*/
+function email_exist($link, $email){
+    $exist = false;
+    $sql = "SELECT email FROM users WHERE email = '$email'";
+    $result = mysqli_query($link, $sql);
+
+    if( $result && mysqli_num_rows($result) > 0){
+        $exist = true;
+    }
+
+    return $exist;
+
+}
